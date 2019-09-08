c53fff460d8f9025e3c466929b0e08a6f1ce3985
DFXCEE <zhbit_ru_hjf@hotmail.com>
2019-09-08 23:50:18 +0800

    Add unit tests.

---
+++ ScrapyCore.Tests/Core/Configure/UserAgentsConfigureTests.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScrapyCore.Tests.Core.Configure
+{
+    public class UserAgentsConfigureTests
+    {
+       
+    }
+}
---
+++ ScrapyCore.Tests/Core/Storages/LocalFileSystemStorageTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using ScrapyCore.Core.Storages;
+using Xunit;
+
+namespace ScrapyCore.Tests.Core.Storages
+{
+    public class LocalFileSystemStorageTests
+    {
+        const string expect = "This is a local file system mock data.";
+        LocalFileSystemStorage fileSystemStorage;
+
+        public LocalFileSystemStorageTests()
+        {
+            string location = Assembly.GetCallingAssembly().Location;
+            fileSystemStorage = new LocalFileSystemStorage(Path.GetDirectoryName(location));
+        }
+
+        [Fact]
+        public void GetStringTest()
+        {
+            var testStr = fileSystemStorage.GetString("MockData/Core/Storage/localfsmockdata.txt");
+            Assert.Equal(expect, testStr);
+        }
+
+        [Fact]
+        public async Task  GetStringAsyncTest()
+        {
+            var testStr = await fileSystemStorage.GetStringAsync("MockData/Core/Storage/localfsmockdata.txt");
+            Assert.Equal(expect,testStr);
+        }
+
+    }
+}
