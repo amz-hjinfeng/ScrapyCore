07b3dfde60822e6d1d3bfe443d402ea0e8d9e3d0
DFXCEE <zhbit_ru_hjf@hotmail.com>
2019-11-19 00:04:59 +0800

    add deploy spec

---
+++ ScrapyCore.HeartOfSwarm/DeployScript/Linux/install_dependencies.sh
@@ -0,0 +1,19 @@
+﻿#!/bin/sh
+
+yum install supervisor -y
+
+cat << EOF >  /ect/supervisor/conf.d/heartofswarm.conf
+[program:heartofswarm]
+command=dotnet ScrapyCore.HeartOfSwarm.dll  #要执行的命令
+directory=/opt/applications/heartofswarm #命令执行的目录
+environment=ASPNETCORE__ENVIRONMENT=Production #环境变量
+user=root  #进程执行的用户身份
+stopsignal=INT
+autostart=false #是否自动启动
+autorestart=true #是否自动重启
+startsecs=1 #自动重启间隔
+stderr_logfile=/var/log/HeartOfSwarm.err.log #标准错误日志
+stdout_logfile=/var/log/HeartOfSwarm.out.log #标准输出日志
+EOF
+
+supervisorctl shutdown && supervisord -c /etc/supervisor/supervisord.conf
---
+++ ScrapyCore.HeartOfSwarm/DeployScript/Linux/stop_server.sh
@@ -0,0 +1,2 @@
+﻿#!/bin/sh
+service supervisor stop
